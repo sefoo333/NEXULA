1d8fb0c702c9d499c79e7d770ef28ee7de07d858
sefoo333 <seifali101z@gmail.com>
2026-08-13 07:29:39 +0300

    update

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -37,19 +37,19 @@ function Hero() {
 
         <div className="counter flex gap-5 max-md:gap-2 mt-7 justify-center px-12 max-md:px-6 bg-gray-950/40 rounded-full backdrop-blur-sm py-3 max-md:py-2">
           <div className="days flex flex-col items-center justify-center px-3 max-md:px-2">
-            <span className="text-2xl max-md:text-lg font-bold text-white">{days}</span>
+            <span className="text-2xl max-md:text-lg font-bold text-white">{0}</span>
             <span className="text-white max-md:text-sm">Days</span>
           </div>
           <div className="days flex flex-col items-center justify-center px-3 max-md:px-2">
-            <span className="text-2xl max-md:text-lg font-bold text-white">{hours}</span>
+            <span className="text-2xl max-md:text-lg font-bold text-white">{0}</span>
             <span className="text-white max-md:text-sm">Hours</span>
           </div>
           <div className="days flex flex-col items-center justify-center px-3 max-md:px-2">
-            <span className="text-2xl max-md:text-lg font-bold text-white">{minutes}</span>
+            <span className="text-2xl max-md:text-lg font-bold text-white">{0}</span>
             <span className="text-white max-md:text-sm">Minutes</span>
           </div>
           <div className="days flex flex-col items-center justify-center px-3 max-md:px-2">
-            <span className="text-2xl max-md:text-lg font-bold text-white">{seconds}</span>
+            <span className="text-2xl max-md:text-lg font-bold text-white">{0}</span>
             <span className="text-white max-md:text-sm">Seconds</span>
           </div>
         </div>
