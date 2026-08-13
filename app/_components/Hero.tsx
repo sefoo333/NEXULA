6a28430bb66db445994d6894242fb0d4615d4a2c
sefoo333 <seifali101z@gmail.com>
2026-08-13 07:34:46 +0300

    update

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -18,10 +18,10 @@ function Hero() {
   }, []);
 
 
-  const seconds = Math.floor((time / 1000) % 60);
-  const minutes = Math.floor((time / (1000 * 60)) % 60);
-  const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  // const seconds = Math.floor((time / 1000) % 60);
+  // const minutes = Math.floor((time / (1000 * 60)) % 60);
+  // const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+  // const days = Math.floor(time / (1000 * 60 * 60 * 24));
 
 
   return (
